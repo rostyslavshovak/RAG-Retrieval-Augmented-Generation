1b045c84cbc4738f776a082eb45f2d0bd701138a
Rostyslav Shovak <96196164+rostyslavshovak@users.noreply.github.com>
2025-01-27 23:24:34 +0200

    Initial commit

--- src/ragas_eval.py
+++ src/ragas_eval.py
@@ -3,65 +3,53 @@ import yaml
 import pandas as pd
 import ragas
 from ragas import EvaluationDataset
+from pathlib import Path
+from ragas.metrics import AnswerRelevancy, AnswerCorrectness
+from inference import answer_query
 
-from ragas.metrics import (
-    AnswerRelevancy,
-    AnswerCorrectness,
-    FactualCorrectness,
-    Faithfulness,
-    LLMContextRecall,
-)
-
-from src.inference import answer_query
 import dotenv
 dotenv.load_dotenv()
 RAGAS_APP_TOKEN = os.getenv("RAGAS_APP_TOKEN")
 
 def main():
-    data_file = os.path.join("../data", "eval_data.yaml")
-    with open(data_file, "r", encoding="utf-8") as f:
-        qa_list = yaml.safe_load(f)
+    # data_file = os.path.join("data", "eval_data.yaml")
+    # with open(data_file, "r") as f:
+    #     qa_pairs = yaml.safe_load(f)
+    current_dir = Path(__file__).parent  # src directory
+    data_path = current_dir.parent / "data" / "eval_data.yaml"
+
+    with open(data_path, "r") as f:
+        qa_pairs = yaml.safe_load(f)
 
     test_samples = []
-    for item in qa_list:
+    for item in qa_pairs:
         user_input = item["user_input"]
         reference = item["expected_response"]
 
-        answer, retrieved_context = answer_query(user_input, history=[], collection_name="merged_context")
-
+        answer, _ = answer_query(
+            item["user_input"],
+            history=[],
+            collection_name="deee"
+        )
         test_samples.append({
             "user_input": user_input,
             "response": answer,
-            "retrieved_contexts": [retrieved_context],
             "reference": reference
         })
 
     df = pd.DataFrame(test_samples)
-
-    expected_columns = {"user_input", "response", "retrieved_contexts", "reference"}
-    if not expected_columns.issubset(df.columns):
-        missing = expected_columns - set(df.columns)
-        print(f"Error: Missing columns in DataFrame: {missing}")
-        return
-
     dataset = EvaluationDataset.from_pandas(df)
 
     metrics = [
         AnswerRelevancy(),
-        AnswerCorrectness(),
-        FactualCorrectness(),
-        Faithfulness(),
-        LLMContextRecall()
+        AnswerCorrectness()
     ]
 
     result = ragas.evaluate(dataset, metrics=metrics)
 
-    print("     RAGAS EVALUATION SCORES     ")
-    if hasattr(result, "results"):
-        for metric_name, val in result.results.items():
-            print(f"{metric_name}: {val:.4f}")
-    else:
-        print(result)
+
+    print("\nRAGAS Evaluation Scores:")
+    print(result.to_pandas().mean(numeric_only=True))
 
     upload_result = result.upload()
     print("RAGAS results can be viewed at:")
